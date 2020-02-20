169867c0e58abb0fbe9f2a699cd0f61d27e2c937
mdespe <mikespe23@gmail.com>
2020-02-20 14:33:10 -0600

    step 3

--- app/src/main/java/com/example/lab4milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab4milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startThread(View view) {
+
+    }
+
+    public void stopThread(View view) {
+
     }
 }
